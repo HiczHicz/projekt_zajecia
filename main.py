d84a58a9c6281f0fdd6a1afee509385e3072f0d9
Tynka <martyna05p@gmail.com>
2025-01-29 14:55:38 +0100

    pol_ang

--- main.py
+++ main.py
@@ -1,16 +1,87 @@
-# This is a sample Python script.
+from tkinter import *
+from tkinter import font
+import random
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+l = []
+with open('slowka.txt', 'r', encoding='utf-8') as slowka:
+    for s in slowka:
+        d = s.strip()
+        r = d.split('\t')
+        l.append(r)
+# polski-angielski
+pol_ang = {}
+for i in l:
+    pol_ang[i[1]] = i[0]
+ang_pol = {}
+for i in l:
+    ang_pol[i[0]] = i[1]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+
+# # angielski-polski
+# angielski = random.choice(list(d.keys()))
+
+def otwieranie_angielski():
+    okno1.destroy()
+    okno_angielski=Tk()
+
+    okno_angielski.title('Tłumaczenie angielskich słowek na polski')
+    slowko = Label(okno_angielski, text='', font=('Comic Sans MS', 20), fg='black')
+    slowko.pack()
+
+    okno_angielski.mainloop()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def otwieranie_polski():
+    def slowko_pol_ang():
+        global polski
+        polski = random.choice(list(pol_ang.keys()))
+        slowko.configure(text=polski)
+        wpisz.delete(0,END)
+    def sprawdz():
+        odpowiedz=wpisz.get()
+        if odpowiedz.lower() == pol_ang[polski].lower():
+            wynik.configure(text='Dobrze!')
+            del pol_ang[polski]
+            wpisz.delete(0, END)
+            slowko.configure(text=polski)
+            slowko_pol_ang()
+        else:
+            wynik.configure(text='źle!')
+    okno1.destroy()
+    okno_polski=Tk()
+    okno_polski.title('Tłumaczenie angielskich słowek na polski')
+    slowko = Label(okno_polski, text='', font=('Comic Sans MS', 20), fg='black')
+    slowko.pack()
+    wynik=Label(okno_polski, text='')
+    wynik.pack()
+    wpisz=Entry()
+    wpisz.pack()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    losuj=Button(text='losuj', command=slowko_pol_ang)
+    losuj.pack()
+
+    ok=Button(text='ok', command=sprawdz)
+    ok.pack()
+
+    okno_polski.mainloop()
+
+
+okno1=Tk()
+okno1.title('Wybierz tryb gry')
+okno1.geometry('750x250')
+okno1.attributes("-topmost", True)
+
+napis=Label(okno1,text='Wybierz tryb gry:', font=('Comic Sans MS', 20), fg='black')
+napis.pack()
+
+
+angpol=Button(okno1,text='Angielski -> Polski',font=('Comic Sans MS', 12), fg = "black", width=25, command=otwieranie_angielski)
+angpol.pack(pady=20)
+
+
+polang=Button(okno1,text='Polski -> Angielski', font=('Comic Sans MS', 12), fg='black', width=25, command=otwieranie_polski)
+polang.pack(pady=20)
+
+
+okno1.mainloop()
